3355a41db4453263dd84b9bf444d669d400b90bb
Apridev <apriantofajar11@gmail.com>
2022-06-04 15:09:36 +0900

    template app

---
+++ lib/template.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+Color primaryColor = Color(0xff143F6B);
+Color redShanksColor = Color(0xffF55353);
+Color blueFbColor = Color(0xff2F65CF);
+Color yellowPopColor = Color(0xffFEB139);
+Color blackTextColor = Color(0xff383838);
+Color backgroundColor = Color(0xffffffff);
+Color backgroundColor2 = Color(0xffD9D9D9);
+Color backgroundColor3 = Color(0xffB0B0B0);
+
+double defaultMargin = 20.0;
+
+TextStyle primaryTextStyle = GoogleFonts.poppins(
+  color: primaryColor
+);
+
+TextStyle redShanksTextStyle = GoogleFonts.poppins(
+  color: redShanksColor
+);
+
+TextStyle yellowPopTextStyle = GoogleFonts.poppins(
+  color: yellowPopColor
+);
+
+TextStyle blackTextStyle = GoogleFonts.poppins(
+  color: blackTextColor
+);
+
+TextStyle backgroundTextStyle = GoogleFonts.poppins(
+  color: backgroundColor
+);
+
+TextStyle backgroundTextStyle2 = GoogleFonts.poppins(
+  color: backgroundColor2
+);
+
+FontWeight reguler = FontWeight.w400;
+FontWeight medium = FontWeight.w500;
+FontWeight semiBold = FontWeight.w600;
+FontWeight bold = FontWeight.w700;
