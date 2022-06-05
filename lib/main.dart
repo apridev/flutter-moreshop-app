a5f9fbf4213c5be31226ca24412a83ea41cc0f87
Apridev <apriantofajar11@gmail.com>
2022-06-05 15:21:16 +0900

    main page

--- lib/main.dart
+++ lib/main.dart
@@ -4,8 +4,10 @@ import 'package:moreshop_app/pages/login-page.dart';
 import 'package:moreshop_app/pages/onboarding.dart';
 import 'package:moreshop_app/pages/recovery-password.dart';
 import 'package:moreshop_app/pages/register-page.dart';
+import 'package:moreshop_app/providers/hidden-password.dart';
 import 'package:moreshop_app/views/privasi-kami.dart';
 import 'package:moreshop_app/views/syarat-kebijakan.dart';
+import 'package:provider/provider.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
@@ -19,15 +21,20 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-     routes: {
-       '/' : (context) => OnBoardingPage(),
-       '/login-page' :(context) => LoginPage(),
-       '/register-page' :(context) => RegisterPage(),
-       '/recovery-password' :(context) => RecoveryPassword(),
-       '/syarat-kebijakan' :(context) => SyaratKebijakan(),
-       '/privasi-kami' :(context) => PrivasiKami(),
-     },
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (context) => HiddenPassword(),)
+      ],
+      child: MaterialApp(
+       routes: {
+         '/' : (context) => OnBoardingPage(),
+         '/login-page' :(context) => LoginPage(),
+         '/register-page' :(context) => RegisterPage(),
+         '/recovery-password' :(context) => RecoveryPassword(),
+         '/syarat-kebijakan' :(context) => SyaratKebijakan(),
+         '/privasi-kami' :(context) => PrivasiKami(),
+       },
+      ),
     );
   }
 }
