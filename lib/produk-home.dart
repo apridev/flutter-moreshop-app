d6eb23508ff7006345802a11e7c66688adc6c0b7
Apridev <apriantofajar11@gmail.com>
2022-06-07 14:06:28 +0900

    data product

---
+++ lib/produk-home.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+final List<Map<String, dynamic>> myList = [
+  {
+    "namaProduk": "Hoodie Navy Blue H&M",
+    "harga":  399000,
+    "rating": "1261",
+    "gambar": "assets/img/product1.png"
+  },
+  {
+    "namaProduk": "Kemeja Flannel Uniqlo",
+    "harga":  499000 ,
+    "rating": "3214",
+    "gambar": "assets/img/product2.png"
+  },
+  {
+    "namaProduk": "Jaket Jeans Hurley",
+    "harga":  399000,
+    "rating": "2314",
+    "gambar": "assets/img/product3.png"
+  },
+];
