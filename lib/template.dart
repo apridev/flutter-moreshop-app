8e716aa709f588fc3994e2f8940e6c665abf8d9e
Apridev <apriantofajar11@gmail.com>
2022-06-07 14:06:13 +0900

    template apps

--- lib/template.dart
+++ lib/template.dart
@@ -9,6 +9,8 @@ Color blackTextColor = Color(0xff383838);
 Color backgroundColor = Color(0xffffffff);
 Color backgroundColor2 = Color(0xffD9D9D9);
 Color backgroundColor3 = Color(0xffB0B0B0);
+Color searchColor = Color(0xffB2B1B5);
+Color categoryColor = Color(0xffAAAAAA);
 
 double defaultMargin = 20.0;
 
@@ -34,6 +36,14 @@ TextStyle backgroundTextStyle = GoogleFonts.poppins(
 
 TextStyle backgroundTextStyle2 = GoogleFonts.poppins(
   color: backgroundColor2
+);
+
+TextStyle searchTextStyle = GoogleFonts.poppins(
+  color: searchColor
+);
+
+TextStyle categoryTextStyle = GoogleFonts.poppins(
+  color: categoryColor
 );
 
 FontWeight reguler = FontWeight.w400;
