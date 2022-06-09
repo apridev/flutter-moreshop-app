bf87473a2fa9cb6ae948c4e4096023e221b68796
Apridev <apriantofajar11@gmail.com>
2022-06-09 12:26:56 +0900

    notification page

---
+++ lib/home/notification-page.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:moreshop_app/template.dart';
+
+class NotificationPage extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: backgroundColor,
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Center(
+            child: Text('Notification Page'),
+          ),
+        ],
+      ),
+    );
+  }
+}
